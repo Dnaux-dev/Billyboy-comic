fcdaa812c8fa19be3a27372cdb6753847af9e15c
Ajilore Daniel <94573586+Dnaux-dev@users.noreply.github.com>
2023-03-23 11:03:02 -0700

    BillyBoy

---
+++ js/main.js
@@ -0,0 +1,159 @@
+// Exclusive Swiper
+var swiper = new Swiper(" .exclusive-content",  {
+  slidesPerView:1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 6000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    280:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758:{
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900:{
+      slidesPerView:4,
+      spaceBetween:20,
+    },
+  },
+});
+
+// Recent release
+var swiper = new Swiper(" .recent-content",  {
+  slidesPerView:1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    280:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758:{
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900:{
+      slidesPerView:4,
+      spaceBetween:20,
+    },
+  },
+});
+
+// Recommend Section
+var swiper = new Swiper(" .recommend-content",  {
+  slidesPerView:1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 11555,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    280:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758:{
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900:{
+      slidesPerView:4,
+      spaceBetween:20,
+    },
+  },
+});
+
+// Coming Soon
+var swiper = new Swiper(" .coming-content",  {
+  slidesPerView:1,
+  spaceBetween: 10,
+  autoplay: {
+    delay: 7555500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    280:{
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    320:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    510:{
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    758:{
+      slidesPerView: 3,
+      spaceBetween: 15,
+    },
+    900:{
+      slidesPerView:4,
+      spaceBetween:20,
+    },
+  },
+});
